034042ec5e8b5093ecdd984229639c112c4bf2b1
rianna bansal <riannabansal126@gmail.com>
2025-11-01 22:22:48 +0530

    changes

--- riannaaurdjango/views.py
+++ riannaaurdjango/views.py
@@ -5,7 +5,7 @@ from . import views
 
 def home(request):
   #return HttpResponse("hello world. You are at rianna and django home page")
-  return render(request, 'website/index.html')
+  return render(request, "website/index.html")
 
 def about(request):
     return HttpResponse("hello world. You are at rianna and django about page")
