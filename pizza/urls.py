bc0479e3aa1fbcfebf0318052cdd804085963fb5
rianna bansal <riannabansal126@gmail.com>
2025-11-07 22:58:29 +0530

    changes

---
+++ pizza/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+#localhost:8000/pizza
+#localhost:8000/pizza/order
+urlpatterns = [
+    path('',views.all_pizza , name='all_pizza'),
+    path('order',views.all_pizza , name='order'),
+]
--- riannaaurdjango/urls.py
+++ riannaaurdjango/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('',views.home , name='home'),
     path('about/',views.about , name='about'),
     path('contact/',views.contact , name='contact'),
-    path('pizza/',views.contact , name='contact'),
+    path('pizza/', include('pizza.urls')),
 ]
